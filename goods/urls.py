8bae3defac45762236ee6facb2562f3a85eb7f0d
SergeyFoma <96164920+SergeyFoma@users.noreply.github.com>
2024-11-27 10:22:32 +0500

    Delete goods directory

--- goods/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from goods import views
-
-app_name='goods'
-
-urlpatterns=[
-	path('<slug:category_slug>/', views.catalog, name='index'),
-	path('products/<slug:product_slug>/', views.products, name='products'),
-]
