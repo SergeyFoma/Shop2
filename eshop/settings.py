0487dbccb94d9be9419b4783c5177a08f8a03141
SergeyFoma <asdfg2105c@gmail.com>
2025-10-31 18:05:25 +0500

    create templates base.html static

--- eshop/settings.py
+++ eshop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base.html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
