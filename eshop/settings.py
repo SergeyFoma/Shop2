74766079d97e3fc9b56e8def4cdf34d181eaca2a
SergeyFoma <asdfg2105c@gmail.com>
2025-10-31 18:14:28 +0500

    settings update templates, index extends base, base link static

--- eshop/settings.py
+++ eshop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'base.html'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
