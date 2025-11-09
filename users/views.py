8837f8b3538b82eb715ad068c7549067f32164a0
SergeyFoma <asdfg2105c@gmail.com>
2025-11-09 08:25:14 +0500

    registr

--- users/views.py
+++ users/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from users.forms import UserRegisterForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 
 def login(request):
     context = {
@@ -7,10 +13,18 @@ def login(request):
     return render(request, "users/login.html", context)
 
 def registration(request):
+    if request.method == "POST":
+        form = UserRegisterForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegisterForm()
     context = {
-
+        'form':form,
     }
-    return render(request, "users/registrations.html", context)
+    return render(request, "users/registration.html", context)
+    
 
 def profile(request):
     context = {
@@ -18,5 +32,7 @@ def profile(request):
     }
     return render(request, "users/profile.html", context)
 
+@login_required
 def logout(request):
-    ...
+    logout(request)
+
