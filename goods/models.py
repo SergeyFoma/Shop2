4ada960c5fe5b3024b7dc8a8416d7bb07b0a6007
SergeyFoma <asdfg2105c@gmail.com>
2025-11-14 10:14:33 +0500

    menu filter update image css

--- goods/models.py
+++ goods/models.py
@@ -14,7 +14,7 @@ class Categories(models.Model):
 class Products(models.Model):
     name = models.CharField(max_length = 150, unique=True, verbose_name='Название')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    image = models.ImageField(upload_to="images/",verbose_name='Изображение', default = 'default.png')
+    image = models.ImageField(upload_to="images/",verbose_name='Изображение', blank=True, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
     discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка")
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
