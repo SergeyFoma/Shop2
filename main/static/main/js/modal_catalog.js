8b05497eaa2a4dcaaf926c2a99210ad0f8ac8f5b
SergeyFoma <asdfg2105c@gmail.com>
2025-11-15 07:49:43 +0500

    create and conection modal_catalog.js

---
+++ main/static/main/js/modal_catalog.js
@@ -0,0 +1,16 @@
+
+//document.getElementById('m_cat').onclick='mod';
+document.getElementById('but2').style.display='none';
+
+function mod() {
+    document.getElementById('m_cat').style.display="block";
+    document.getElementById('but2').style.display='block';
+    document.getElementById('but1').style.display='none';
+}
+
+function mod2(){
+    document.getElementById('m_cat').style.display="none";
+    document.getElementById('but2').style.display='none';
+    document.getElementById('but1').style.display='block';
+}
+    
